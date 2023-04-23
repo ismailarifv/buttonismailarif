02f230b3bfb151d30d9ac70b0cd082402aa49160
ismailarifv <i.arifgulec8110@gmail.com>
2023-04-23 17:41:43 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'buttonismailarif'
+import 'buttonismailarif/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
